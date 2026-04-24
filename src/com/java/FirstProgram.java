19694c18451aeb5f3b4b737348aab9f9b46e1506
Harsharsaler89 <harshasaler7@gmail.com>
2026-04-24 12:19:13 +0530

    Commit Message

--- src/com/java/FirstProgram.java
+++ src/com/java/FirstProgram.java
@@ -2,6 +2,6 @@ package com.java;
 
 public class FirstProgram {
 void print() {
-	System.out.println("Welcome back to github session2");
+	System.out.println("Welcome back to Github session2");
 }
 }
